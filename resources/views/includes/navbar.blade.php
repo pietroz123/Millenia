98d256ae32980b4d2d4ce3de2cb4d509f181b01d
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-11-05 23:10:19 -0300

    Adição do logo

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('img/icons/logo.svg') }}" class="navbar-brand-logo" alt="Ícone Millenia">
             Millenia
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
