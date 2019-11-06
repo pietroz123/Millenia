5db707dce43b406348ddd4fce7eb227f211076e7
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-11-05 23:56:15 -0300

    Inputs material design
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,29 +30,31 @@
             @endif
         </p>
 
-        <form method="POST" action="{{ route('login') }}" class="mt-5">
+        <form method="POST" action="{{ route('login') }}">
             @csrf
         
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-        
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        
-            <div class="form-group">
-                <label for="password">Senha</label>
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-        
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
+            <div class="my-5">
+                <div class="md-form">
+                    <label for="email">Email</label>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                
+                <div class="md-form">
+                    <label for="password">Senha</label>
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
             </div>
         
             <div class="form-group">
