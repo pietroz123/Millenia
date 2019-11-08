615369673f7e90e0d8c184ce90b8dd9a596ae6b9
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-11-07 23:51:34 -0300

    Fix nome

--- database/migrations/2019_11_08_022713_create_profissoes_table.php
+++ database/migrations/2019_11_08_022713_create_profissoes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProfissaosTable extends Migration
+class CreateProfissoesTable extends Migration
 {
     /**
      * Run the migrations.
