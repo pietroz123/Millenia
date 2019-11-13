cca84ca32fc353a4a4590d74c562c1a44c293a03
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-11-13 11:38:15 -0300

    Ajuste na migração de clientes para cidade e estado
    Co-authored-by: Pietro Zuntini <pietrozuntini@gmail.com>

--- database/migrations/2019_11_08_024428_create_clientes_table.php
+++ database/migrations/2019_11_08_024428_create_clientes_table.php
@@ -17,19 +17,23 @@ class CreateClientesTable extends Migration
             $table->bigIncrements('id');
             $table->string('nome');
             $table->string('email');
-            $table->string('rua');
-            $table->integer('numero_rua');
-            $table->string('complemento_rua');
+            $table->bigInteger('id_cidade')->unsigned();
+            $table->bigInteger('id_estado')->unsigned();
             $table->string('cep');
             $table->string('bairro');
+            $table->string('rua');
+            $table->string('complemento_rua')->nullable();
             $table->string('telefone_celular');
-            $table->string('telefone_residencial');
+            $table->string('telefone_residencial')->nullable();
             $table->integer('pontuacao')->default(0);
             $table->bigInteger('id_profissao')->unsigned();
+            $table->boolean('deseja_notificacao');
             $table->timestamps();
         });
         Schema::table('clientes', function (Blueprint $table) {
             $table->foreign('id_profissao')->references('id')->on('profissoes')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_cidade')->references('id')->on('cidades')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('id_estado')->references('id')->on('estados')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
