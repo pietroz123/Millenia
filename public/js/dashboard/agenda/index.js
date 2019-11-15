b7c5dc4c525647dc26669eba1837786db2eab5d0
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-11-14 23:47:37 -0300

    Ajustes no inpit de cor
    Co-authored-by: Bianca Gomes <bgsrd@outlook.com>

--- public/js/dashboard/agenda/index.js
+++ public/js/dashboard/agenda/index.js
@@ -29,6 +29,23 @@ $(document).ready(function() {
         ],
         defaultView: 'timeGridWeek',
         height: 800,
+        events: [
+            {
+                title: '[Corte Masculino] João',
+                start: '2019-11-12T14:30:00',
+                end: '2019-11-12T15:00:00',
+                extendedProps: {
+                    status: 'done'
+                }
+            },
+            {
+                title: 'Birthday Party',
+                start: '2019-11-13T07:00:00',
+                backgroundColor: 'green',
+                borderColor: 'green',
+            }
+        ],
+        eventTextColor: 'white',
     });
     calendar.render();
 
