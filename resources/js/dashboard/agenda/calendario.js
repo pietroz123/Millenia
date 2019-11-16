be1654cf076dc52b895fd67598b93b892cf0604c
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-11-16 00:08:06 -0300

    Rota de API para clientes, recupera clientes para  agendamento
    Co-authored-by: Bianca Gomes <bgsrd@outlook.com>

--- resources/js/dashboard/agenda/calendario.js
+++ resources/js/dashboard/agenda/calendario.js
@@ -142,6 +142,7 @@ $(document).ready(function() {
     
                 const profissionais = retorno;
     
+                // Cria as opções
                 var options = [];
                 for (let i = 0; i < profissionais.length; i++) {
                     const profissional = profissionais[i];
@@ -154,6 +155,7 @@ $(document).ready(function() {
                     );
                 }
     
+                // Cria o select de resposta
                 if (options.length > 0) {
                     $('#selecionar-profissional').html(
                         '<div class="form-group mt-2">' +
@@ -189,7 +191,57 @@ $(document).ready(function() {
      */
     $(document).on('change', 'select#profissional', function() {
 
-        
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            url: '/api/clientes',
+            method: 'GET',
+            success: function(retorno) {
+
+                const clientes = retorno.data;
+                
+                // Cria as opções
+                var options = [];
+                for (let i = 0; i < clientes.length; i++) {
+                    const cliente = clientes[i];
+                    const id = cliente.id;
+                    const nome = cliente.nome;
+                    options.push(
+                        '<option value="'+ id +'">' +
+                            '<span>'+ nome +'</span>' +
+                        '</option>'
+                    );
+                }
+    
+                // Cria o select de resposta
+                if (options.length > 0) {
+                    $(
+                        '<div class="form-group mt-2">' +
+                            '<label for="cliente">Selecionar o cliente</label>' +
+                            '<select class="browser-default custom-select" id="cliente" name="cliente">' +
+                                '<option></option>' +
+                                options +
+                            '</select>' +
+                        '</div>'
+                    ).insertAfter('#selecionar-profissional');
+                    $('select#cliente').select2({
+                        placeholder: 'Selecione o cliente',
+                    });
+                }
+                else {
+                    $(
+                        '<div class="alert alert-info" role="alert">' +
+                            'Não encontramos nenhum cliente com esse serviço.' +
+                        '</div>'
+                    ).insertAfter('#selecionar-profissional');
+                }
+            },
+            error: function(retorno) {
+                console.log('Error');
+                console.log(retorno);
+            }
+        });
 
     });
     
