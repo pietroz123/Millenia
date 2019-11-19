4386c44ff1562f2274ba077774ec8ab07094447d
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-11-19 00:14:45 -0300

    Buscar comandas pelo nome do cliente
    Co-authored-by: Bianca Gomes <bgsrd@outlook.com>

--- app/Http/Livewire/ListaComandas.php
+++ app/Http/Livewire/ListaComandas.php
@@ -4,14 +4,29 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Comanda;
+use App\Cliente;
 
 class ListaComandas extends Component
 {
     public $abertas;
     public $comandas;
+    public $query;
+
+    public function updatedQuery()
+    {
+        $this->comandas = Comanda::whereIn('id_cliente',
+        
+            // IDs de clientes com o trecho do nome
+            Cliente::where('nome', 'like', '%' . $this->query . '%')->pluck('id')->toArray()
+
+        )->where('aberta', $this->abertas)
+            ->get()
+            ->toArray();
+    }
 
     public function mount()
     {
+        $this->query = '';
         $this->abertas = true;
         $this->comandas = Comanda::all()->where('aberta', true)->toArray();
     }
--- resources/views/livewire/lista-comandas.blade.php
+++ resources/views/livewire/lista-comandas.blade.php
@@ -4,34 +4,50 @@
         <div class="col-6">
             <label for="tipo-comanda">Tipo de Comanda</label>
             <select class="browser-default custom-select" id="tipo-comanda" wire:model="abertas">
-                {{-- <option>Selecione o tipo de comanda (Abertas/Fechadas)</option> --}}
                 <option selected value="1">Abertas</option>
                 <option value="0">Fechadas</option>
             </select>    
         </div>
     </div>
 
-    <div class="comandas mt-4">
-        @foreach ($comandas as $comanda)
-            @php
-                $comanda = App\Comanda::find($comanda['id'])
-            @endphp
-            <a href="{{ route('comandas.edit', $comanda->id) }}" class="comanda">
-                <div class="header">
-                    {{ $comanda->cliente->nome }}
-                </div>
-                <div class="info-basica-comanda">
-                    {{-- <span class="font-weight-bold">Cliente</span> <span class="valor">{{ $comanda->cliente->nome }}</span> --}}
-                    <span class="font-weight-bold">Data</span> <span class="valor">{{ date('d/m/Y', $comanda->created_at->timestamp) }}</span>
-                </div>
-                <div class="servicos-comanda">
-                    <ul class="list-group">
-                        @foreach ($comanda->servicos as $servico)
-                            <li class="list-group-item">{{ $servico->nome }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            </a>
-        @endforeach
+    <div class="crud-tool-bar">
+        <div class="input-group mt-3 mb-2">
+            <div class="input-group-prepend">
+                <span class="input-group-text">
+                    <i class="fas fa-search"></i>
+                </span>
+            </div>
+            <input type="text" name="buscar-cliente" id="buscar-cliente" class="form-control" placeholder="Buscar cliente..." autocomplete="new" wire:model="query">
+        </div>
     </div>
+
+    @if (!empty($comandas))
+        <div class="comandas mt-4">
+            @foreach ($comandas as $comanda)
+                @php
+                    $comanda = App\Comanda::find($comanda['id'])
+                @endphp
+                <a href="{{ route('comandas.edit', $comanda->id) }}" class="comanda">
+                    <div class="header">
+                        {{ $comanda->cliente->nome }}
+                    </div>
+                    <div class="info-basica-comanda">
+                        {{-- <span class="font-weight-bold">Cliente</span> <span class="valor">{{ $comanda->cliente->nome }}</span> --}}
+                        <span class="font-weight-bold">Data</span> <span class="valor">{{ date('d/m/Y', $comanda->created_at->timestamp) }}</span>
+                    </div>
+                    <div class="servicos-comanda">
+                        <ul class="list-group">
+                            @foreach ($comanda->servicos as $servico)
+                                <li class="list-group-item">{{ $servico->nome }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    @else
+        <div class="alert alert-info mt-3" role="alert">
+            Não foi encontrada nenhuma comanda.
+        </div>
+    @endif
 </div>
