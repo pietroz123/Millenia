99e9416188456d1375abe159a4b3b7feeb46f13f
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2019-11-19 01:07:35 -0300

    Ideia de página principal da dashbaord
    Co-authored-by: Bianca Gomes <bgsrd@outlook.com>

--- resources/views/dashboard/dashboard-index.blade.php
+++ resources/views/dashboard/dashboard-index.blade.php
@@ -10,4 +10,19 @@
     
     <h3>Dashboard de {{ Auth::user()->name }}</h3>
 
+    <div class="d-flex flex-row mt-5">
+
+        <div class="info-card">
+            <span class="name">Clientes (<a href="{{ route('clientes.index') }}">visualizar</a>)</span>
+            <span class="value">16 clientes</span>
+            {{-- <span>
+                <span class="stats">
+                    <span class="stats-value decrease">0.1%</span>
+                </span>
+                <span class="stats-info">Desde o último mês</span>
+            </span> --}}
+        </div>
+
+    </div>
+
 @endsection
